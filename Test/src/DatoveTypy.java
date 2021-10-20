af4e58fbef9f91aa0e624f8a0c245bb376e2fb6d
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-20 13:09:25 +0200

    datove typy

---
+++ Test/src/DatoveTypy.java
@@ -0,0 +1,47 @@
+
+public class DatoveTypy {
+
+	public static void main(String[] args) {
+		
+		
+		//celociselne premenne
+		int a = 10;
+		
+		//deklaracia premennej
+		short s;
+		
+		//inicializacia premennej
+		s = 12;
+		
+		System.out.println("a = " + a);
+
+		int bin = 0b10000000;
+		System.out.println(bin);
+		int hex = 0xff;
+		System.out.println(hex);
+		
+		//desatinne cisla
+		double des = 12.54;
+		float fl = 12.4f;
+		
+		
+		
+		byte bb = 12;
+		int ii = bb;
+		
+		//pretypovanie
+		bb = (byte)ii;
+		
+		
+		char znak = '@';
+		znak = 64;
+		
+		System.out.println(znak);
+		
+		boolean prsi = false;
+		
+		
+		
+	}
+
+}
--- Test/src/PrvaTrieda.java
+++ Test/src/PrvaTrieda.java
@@ -1,7 +1,20 @@
 public class PrvaTrieda {
 
 	public static void main(String[] args) {
+		
+		//toto je jednoriadkovy komentar
+		
 		System.out.println("Ahoj 4B");
+		/*
+		 * Toto je 
+		 * viacriadkovy komentar
+		 */
+		System.out.println("Programovanie v Jave");
+		System.out.println("10 + 5");
+		System.out.println("Lukas" + " " + "Fero");
+		System.out.println("10 + " + 5);
+		System.out.println("10 + 5 = " + (10 + 5));
+		
 
 	}
 
