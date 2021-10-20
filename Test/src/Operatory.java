60fb4b89f8d7a20a451d90d8eeb047371bdf08a7
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-20 13:30:17 +0200

    operatory

---
+++ Test/src/Operatory.java
@@ -0,0 +1,22 @@
+
+public class Operatory {
+
+	public static void main(String[] args) {
+		
+		System.out.println(10 + 5);
+		System.out.println(10 - 5);
+		System.out.println(10 / 3.0);
+		System.out.println(10 * 5);
+		System.out.println(10 % 8);
+		
+		System.out.println(10 < 5);
+		System.out.println(10 > 5);
+		System.out.println(10 <= 3.0);
+		System.out.println(10 >=5);
+		System.out.println(10 == 8);
+		System.out.println(10 != 8);
+		
+
+	}
+
+}
