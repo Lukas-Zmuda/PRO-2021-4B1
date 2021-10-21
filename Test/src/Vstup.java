5762d8d93e703e7c34d8cfb11df07437804863b3
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-21 07:49:31 +0200

    vstup

---
+++ Test/src/Vstup.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Vstup {
+
+	public static void main(String[] args) {
+		
+		Scanner vstup = new Scanner(System.in);
+		
+		int rokNarodenia = 0;
+		String meno = "";
+		
+		System.out.println("Ahoj, ako sa volas?");
+		
+		meno = vstup.nextLine();
+		
+		System.out.println("Ahoj " + meno + " . V ktorom roku si sa narodil?");
+		
+		rokNarodenia = vstup.nextInt();
+		
+		System.out.println(meno + ", ty mas uz " + (2021 - rokNarodenia) + " rokov.");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
